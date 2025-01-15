8ceddc33afaa65a0b346be70de583ef7366d4d79
mmadewell <mattmade94@gmail.com>
2025-01-15 14:17:31 -0600

    asteroids CH 2 L 3

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -17,6 +19,7 @@ def main():
         screen.fill(000000)
 
         pygame.display.flip()
+        dt = (clock.tick(60) /1000)
 
 if __name__ == "__main__":
     main()
