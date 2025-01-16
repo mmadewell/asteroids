d7e5de087c21a8fb333fe73a65e48827a29e3b49
mmadewell <mattmade94@gmail.com>
2025-01-16 11:22:32 -0600

    asteroids CH 2 L 8

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 #delta time
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -19,10 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for thing in updatable:
+            thing.update(dt)
 
         screen.fill(000000)
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
         dt = (clock.tick(60) /1000)
